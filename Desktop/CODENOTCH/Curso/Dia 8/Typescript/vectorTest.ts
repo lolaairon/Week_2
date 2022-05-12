c92e35e5c9d7462c084045cdaa6d2de671f6991b
unknown <lolaairon@gmail.com>
2022-05-12 13:13:45 +0200

    Ultimo ejercicio

---
+++ Desktop/CODENOTCH/Curso/Dia 8/Typescript/vectorTest.ts
@@ -0,0 +1,11 @@
+import {Vector} from "./reto8"
+
+let vector1 = new Vector (5,20);
+let v1 = new Vector(5,20);
+let v2 = new Vector(9,100);
+v1.getprint();
+vector1.getprint()
+console.log(vector1.add(v1));
+console.log(vector1.subs(v1));
+console.log(vector1.mult(v1));
+console.log(vector1.multNumer(2));
